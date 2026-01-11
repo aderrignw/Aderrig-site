6a0a66d031b87f1cf4e96af8acd6a008f618b728
aderrignw <aderrignw@gmail.com>
2026-01-11 17:29:08 +0000

    Delete netlify/functions directory

--- netlify/functions/config.js
+++
@@ -1,9 +0,0 @@
-export default async () => {
-  return new Response(
-    JSON.stringify({
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
-    }),
-    { status: 200, headers: { "content-type": "application/json" } }
-  );
-};
