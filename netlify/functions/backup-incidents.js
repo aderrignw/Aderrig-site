46181fe167f13d309bd137944bda30cc292b19b2
aderrignw <aderrignw@gmail.com>
2026-02-18 21:58:56 +0000

    Delete netlify directory

--- netlify/functions/backup-incidents.js
+++
@@ -1,7 +0,0 @@
-// netlify/functions/backup-incidents.js
-// Backwards-compat wrapper. Prefer using backup-scheduled (full snapshot).
-import backupScheduled from "./backup-scheduled.js";
-
-export default async (req, context) => {
-  return backupScheduled(req, context);
-};
